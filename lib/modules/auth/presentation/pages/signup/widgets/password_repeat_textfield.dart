447383f420b97d772fa1a948bc276b73348a8d7a
umutarpat <arptumut@gmail.com>
2025-07-16 16:25:23 +0300

    Added obscure text to register password fields

--- lib/modules/auth/presentation/pages/signup/widgets/password_repeat_textfield.dart
+++ lib/modules/auth/presentation/pages/signup/widgets/password_repeat_textfield.dart
@@ -4,8 +4,24 @@ import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
 
-class SignupPagePasswordRepeatTextField extends StatelessWidget {
+class SignupPagePasswordRepeatTextField extends StatefulWidget {
   const SignupPagePasswordRepeatTextField({super.key});
+
+  @override
+  State<SignupPagePasswordRepeatTextField> createState() =>
+      _SignupPagePasswordRepeatTextFieldState();
+}
+
+class _SignupPagePasswordRepeatTextFieldState
+    extends State<SignupPagePasswordRepeatTextField> {
+  bool _passwordRepeatFieldObscurePassword = true;
+
+  void _togglePasswordRepeatFieldVisibility() {
+    setState(() {
+      _passwordRepeatFieldObscurePassword =
+          !_passwordRepeatFieldObscurePassword;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,10 +46,19 @@ class SignupPagePasswordRepeatTextField extends StatelessWidget {
         dontAllowSpaceInputFormatter,
         dontAllowEmojiInputFormatter,
       ],
+      obscureText: _passwordRepeatFieldObscurePassword,
       decoration: InputDecoration(
         hintText: l10n.repeatPassword,
         filled: true,
         fillColor: Colors.white,
+        suffixIcon: IconButton(
+          onPressed: _togglePasswordRepeatFieldVisibility,
+          icon: Icon(
+            _passwordRepeatFieldObscurePassword
+                ? Icons.visibility
+                : Icons.visibility_off,
+          ),
+        ),
       ),
     );
   }
--- lib/modules/auth/presentation/pages/signup/widgets/password_textfield.dart
+++ lib/modules/auth/presentation/pages/signup/widgets/password_textfield.dart
@@ -6,12 +6,28 @@ import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
 
-class SignupPagePasswordTextField extends StatelessWidget {
+class SignupPagePasswordTextField extends StatefulWidget {
   const SignupPagePasswordTextField({super.key});
+
+  @override
+  State<SignupPagePasswordTextField> createState() =>
+      _SignupPagePasswordTextFieldState();
+}
+
+class _SignupPagePasswordTextFieldState
+    extends State<SignupPagePasswordTextField> {
   // At least 8 characters
   // At least 1 uppercase letter
   // At least 1 number
   // At least 1 special character
+
+  bool _passwordFieldObscurePassword = true;
+
+  void _togglePasswordFieldVisibility() {
+    setState(() {
+      _passwordFieldObscurePassword = !_passwordFieldObscurePassword;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,11 +53,21 @@ class SignupPagePasswordTextField extends StatelessWidget {
           ).extension<DesignTextStyles>()?.fieldErrorTextStyle,
         );
       },
+
       name: "password",
+      obscureText: _passwordFieldObscurePassword,
       decoration: InputDecoration(
         hintText: l10n.password,
         filled: true,
         fillColor: Colors.white,
+        suffixIcon: IconButton(
+          onPressed: _togglePasswordFieldVisibility,
+          icon: Icon(
+            _passwordFieldObscurePassword
+                ? Icons.visibility
+                : Icons.visibility_off,
+          ),
+        ),
       ),
     );
   }
